2261929b5cd3e604da46b9b19829c26dbb61d9dc
BT_2N2_12 <sample@bths.edu>
2023-10-23 13:15:26 -0400

    5

---
+++ src/Point.java
@@ -0,0 +1,35 @@
+public class Point {
+
+
+    private int x;
+    private int y;
+
+public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+public Point (int num) {
+    x = num;
+    y = num;
+    }
+public Point() {
+    x = 0;
+    y = 0;
+    }
+
+public int getX() {
+    return x;
+    }
+public int getY() {
+    return y;
+}
+public void setX(int newX) {
+    x =  newX;
+    }
+public void setY(int newY) {
+    y = newY;
+}
+public String coordinate() {
+    return("("+x+","+y+")");
+}
+}
